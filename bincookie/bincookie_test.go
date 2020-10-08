b3fc5ef666c832f1527c342f7e8e3e2c3b10b584
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-07 19:40:48 -0700

    Add a manual production test.

--- bincookie/bincookie_test.go
+++ bincookie/bincookie_test.go
@@ -2,6 +2,10 @@ package bincookie_test
 
 import (
 	"bytes"
+	"flag"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
@@ -9,6 +13,57 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+var (
+	inputFile  = flag.String("input", "", "Input binarycookies file")
+	outputFile = flag.String("output", "", "Output binarycookies file")
+)
+
+// Manually verify that a "real" user-provided binarycookies file can be
+// round-tripped correctly if no modifications are made.
+//
+// If an -output file is provided, also write the output there so that it can
+// be preserved for later study.
+func TestManual(t *testing.T) {
+	if *inputFile == "" {
+		t.Skip("Skipping test since no -input is specified")
+	}
+	data, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		t.Fatalf("Reading input: %v", err)
+	}
+	t.Logf("Read %d bytes from %q", len(data), *inputFile)
+	f, err := bincookie.ParseFile(data)
+	if err != nil {
+		t.Fatalf("ParseFile failed: %v", err)
+	}
+
+	var buf bytes.Buffer
+	var w io.Writer = &buf
+	if *outputFile != "" {
+		out, err := os.Create(*outputFile)
+		if err != nil {
+			t.Fatalf("Creating output: %v", err)
+		}
+		defer func() {
+			if err := out.Close(); err != nil {
+				t.Fatalf("Closing output: %v", err)
+			}
+		}()
+		w = io.MultiWriter(&buf, out)
+	}
+
+	nw, err := f.WriteTo(w)
+	if err != nil {
+		t.Errorf("Writing output: %v", err)
+	} else {
+		t.Logf("Wrote %d bytes", nw)
+	}
+
+	if diff := cmp.Diff(data, buf.Bytes()); diff != "" {
+		t.Errorf("Incorrect output: (-want, +got):\n%s", diff)
+	}
+}
 
 func TestRoundTrip(t *testing.T) {
 	base := time.Unix(1602034364, 0)
