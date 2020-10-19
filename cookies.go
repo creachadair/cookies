dab91e3c4eac1f6895c18f1b3ff604c06b4d4d74
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-18 22:43:57 -0700

    Add a format-independent cookie representation.

---
+++ cookies.go
@@ -0,0 +1,47 @@
+// Copyright 2020 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cookies reads and modifies browser cookies.
+package cookies
+
+import "time"
+
+// C is a format-independent representation of a browser cookie.
+type C struct {
+	Name   string
+	Value  string
+	Domain string
+	Path   string
+
+	Expires time.Time
+	Created time.Time
+	Flags   Flags
+}
+
+// Flags represents the optional flags that can be set on a cookie.
+type Flags struct {
+	Secure   bool // only send this cookie on an encrypted connection
+	HTTPOnly bool // do not expose this cookie to scripts
+}
+
+// An Editor maps between format-specific representation of a cookie and the
+// format-independent version.
+type Editor interface {
+	// Get returns a format-independent representation of the receiver.
+	Get() *C
+
+	// Set updates the contents of the receiver to match c.
+	// It reports an error if c cannot be represented in the format.
+	Set(c *C) error
+}
