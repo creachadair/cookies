48c1dfa7ad6a1f4388489e1416cc5f79c8bfd0b2
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-21 10:37:02 -0700

    Remove the schema comment.

--- chromedb/chromedb.go
+++ chromedb/chromedb.go
@@ -52,30 +52,6 @@ WHERE rowid = $rowid;`
 	chromeEpoch = 11644473600
 )
 
-/*
-  Schema:
-
-  CREATE TABLE cookies (
-     creation_utc     INTEGER  NOT NULL,
-     host_key         TEXT     NOT NULL,
-     name             TEXT     NOT NULL,
-     value            TEXT     NOT NULL,
-     path             TEXT     NOT NULL,
-     expires_utc      INTEGER  NOT NULL,
-     is_secure        INTEGER  NOT NULL,
-     is_httponly      INTEGER  NOT NULL,
-     last_access_utc  INTEGER  NOT NULL,
-     has_expires      INTEGER  NOT NULL DEFAULT  1,
-     is_persistent    INTEGER  NOT NULL DEFAULT  1,
-     priority         INTEGER  NOT NULL DEFAULT  1,
-     encrypted_value  BLOB              DEFAULT '',
-     samesite         INTEGER  NOT NULL DEFAULT  -1,
-     source_scheme    INTEGER  NOT NULL DEFAULT  0,
-
-     UNIQUE (host_key, name, path)
-);
-*/
-
 // Open opens the Chrome cookie database at the specified path.
 func Open(path string, opts *Options) (*Store, error) {
 	conn, err := sqlite.OpenConn(path, sqlite.SQLITE_OPEN_READWRITE)
